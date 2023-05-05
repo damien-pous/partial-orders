3dca14ef01f0d5cd09c380fe9578c5a2dce02f92
Damien Pous <damien.pous@ens-lyon.fr>
2023-05-05 14:25:30 +0200

    complexity issue

--- tests/sanity_spo.v
+++ tests/sanity_spo.v
@@ -4,3 +4,14 @@ From PartialOrders Require Import spo.
 Goal forall l, sA<<l -> sE <<l.
   intros l L. solve_lower. 
 Qed.
+
+(* Set Debug "unification". *)
+(* Set Printing All. *)
+Time Goal
+  let T := ((bool -> Prop): PO) in
+  let X := ((T-mon->T): SPO sA) in
+  True.
+intros.
+simpl in *.
+(* cbv in T. *)
+Abort.
